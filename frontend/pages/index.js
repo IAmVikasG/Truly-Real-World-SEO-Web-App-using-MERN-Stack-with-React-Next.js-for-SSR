4b7dd43426864437cd4e71cb44c8ac193e66bfd3
Nishant <vikaskumar.e1256@gmail.com>
2024-08-06 22:41:14 +0530

    Basic page layout defined

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Layout from '../components/Layout';
 
 function index(props) {
     return (
-        <div>
+        <Layout>
           Hello World
-        </div>
+        </Layout>
     );
 }
 
