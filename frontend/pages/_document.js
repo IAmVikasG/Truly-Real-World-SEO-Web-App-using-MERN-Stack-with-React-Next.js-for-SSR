03e6c657ab3fbc1c990bc3d2ce201d3633ceddca
Nishant <vikaskumar.e1256@gmail.com>
2024-08-09 18:53:39 +0530

    css using static folder

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -12,6 +12,7 @@ export default function MyDocument()
                     href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css"
                 />
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.css" integrity="sha512-DanfxWBasQtq+RtkNAEDTdX4Q6BPCJQ/kexi/RftcP0BcA4NIJPSi7i31Vl+Yl5OCfgZkdJmCqz+byTOIIRboQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+                <link rel="stylesheet" href='/static/css/styles.css' />
             </Head>
             <body>
                 <Main />
