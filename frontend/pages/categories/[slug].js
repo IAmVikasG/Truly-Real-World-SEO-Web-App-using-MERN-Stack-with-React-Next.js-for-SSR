59c4a5040a664414dda6be8e372c0a347c16cdf3
Nishant <vikaskumar.e1256@gmail.com>
2024-08-12 11:57:01 +0530

    show categories associate with blogs

---
+++ frontend/pages/categories/[slug].js
@@ -0,0 +1,49 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import Layout from '../../components/Layout';
+import { singleCategory } from '../../actions/category';
+import { API, DOMAIN, APP_NAME, FB_APP_ID } from '../../config';
+import moment from 'moment';
+import Card from '../../components/blog/Card';
+
+const Category = ({ category, blogs }) =>
+{
+    return (
+        <>
+            <Layout>
+                <main>
+                    <div className="container-fluid text-center">
+                        <header>
+                            <div className="col-md-12 pt-3">
+                                <h1 className="display-4 font-weight-bold">{category.name}</h1>
+                                {blogs.map((b, i) => (
+                                    <div>
+                                        <Card key={i} blog={b} />
+                                        <hr />
+                                    </div>
+                                ))}
+                            </div>
+                        </header>
+                    </div>
+                </main>
+            </Layout>
+        </>
+    );
+};
+
+Category.getInitialProps = ({ query }) =>
+{
+    return singleCategory(query.slug).then(result =>
+    {
+        const { success, message, data } = result;
+        if (!success)
+        {
+            console.log(message);
+        } else
+        {
+            return { category: data.category, blogs: data.blogs };
+        }
+    });
+};
+
+export default Category;
