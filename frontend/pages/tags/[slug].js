f6784272a626a5c53f7d642e3dab6f6bdfc709fc
Nishant <vikaskumar.e1256@gmail.com>
2024-08-12 12:14:37 +0530

    show tags associate with blogs

--- frontend/pages/tags/[slug].js
+++ frontend/pages/tags/[slug].js
@@ -1,22 +1,22 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import Layout from '../../components/Layout';
-import { singleCategory } from '../../actions/category';
+import { singleTag } from '../../actions/tag';
 import { API, DOMAIN, APP_NAME, FB_APP_ID } from '../../config';
 import moment from 'moment';
 import Card from '../../components/blog/Card';
 
-const Category = ({ category, blogs, query }) =>
+const Tag = ({ tag, blogs, query }) =>
 {
     const head = () => (
         <Head>
             <title>
-                {category.name} | {APP_NAME}
+                {tag.name} | {APP_NAME}
             </title>
-            <meta name="description" content={`Best programming tutorials on ${category.name}`} />
+            <meta name="description" content={`Best programming tutorials on ${tag.name}`} />
             <link rel="canonical" href={`${DOMAIN}/categories/${query.slug}`} />
-            <meta property="og:title" content={`${category.name}| ${APP_NAME}`} />
-            <meta property="og:description" content={`Best programming tutorials on ${category.name}`} />
+            <meta property="og:title" content={`${tag.name}| ${APP_NAME}`} />
+            <meta property="og:description" content={`Best programming tutorials on ${tag.name}`} />
             <meta property="og:type" content="webiste" />
             <meta property="og:url" content={`${DOMAIN}/categories/${query.slug}`} />
             <meta property="og:site_name" content={`${APP_NAME}`} />
@@ -27,6 +27,7 @@ const Category = ({ category, blogs, query }) =>
             <meta property="fb:app_id" content={`${FB_APP_ID}`} />
         </Head>
     );
+
     return (
         <>
             {head()}
@@ -35,7 +36,7 @@ const Category = ({ category, blogs, query }) =>
                     <div className="container-fluid text-center">
                         <header>
                             <div className="col-md-12 pt-3">
-                                <h1 className="display-4 font-weight-bold">{category.name}</h1>
+                                <h1 className="display-4 font-weight-bold">{tag.name}</h1>
                                 {blogs.map((b, i) => (
                                     <div>
                                         <Card key={i} blog={b} />
@@ -51,9 +52,9 @@ const Category = ({ category, blogs, query }) =>
     );
 };
 
-Category.getInitialProps = ({ query }) =>
+Tag.getInitialProps = ({ query }) =>
 {
-    return singleCategory(query.slug).then(result =>
+    return singleTag(query.slug).then(result =>
     {
         const { success, message, data } = result;
         if (!success)
@@ -61,9 +62,9 @@ Category.getInitialProps = ({ query }) =>
             console.log(message);
         } else
         {
-            return { category: data.category, blogs: data.blogs, query };
+            return { tag: data.tag, blogs: data.blogs, query };
         }
     });
 };
 
-export default Category;
+export default Tag;
